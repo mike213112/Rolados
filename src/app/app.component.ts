abeeab5bd54b854a5c75d1eb4a695913c44cc8c8
mike213112 <mikeramirez-14-13@hotmail.com>
2020-07-07 10:30:33 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rolados-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'RoladoEstructurales';
+}
